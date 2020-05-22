674eee1f5bc9cbbbe9dce9b55e5250dc3b807748
Nicolas Dinolfo <nicolas.dinolfio@xpo.com>
2020-05-22 14:06:43 -0300

    better jwt verification

--- src/helpers/verifyToken.ts
+++ src/helpers/verifyToken.ts
@@ -1,17 +1,19 @@
-import * as jwt from 'jwt-then';
-import config from '../config/config';
+import * as jwt from "jwt-then";
+import config from "../config/config";
 const verifyToken = async (req, res, next): Promise<any> => {
   // check header or url parameters or post parameters for token
-  const token: string = req.headers.authorization.split(' ')[1];
+  const token: string = req.headers.authorization.split(" ")[1];
 
   if (!token) {
-    return res.status(403).send({ auth: false, message: 'No token provided.' });
+    return res.status(403).send({ auth: false, message: "No token provided." });
   }
 
   try {
     // verifies secret and checks exp
     const decoded = await jwt.verify(token, config.JWT_ENCRYPTION);
-    req.email = decoded.email;
+    if (req.email !== decoded.email) {
+      res.status(401).send({ auth: false, message: "Email incorrecto" });
+    }
     next();
   } catch (err) {
     res.status(500).send({ auth: false, message: err });
