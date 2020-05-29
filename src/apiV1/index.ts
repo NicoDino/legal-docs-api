0d296ee6a188a413aa6613c57e2d4b0b50ee6e73
Nicolas Dinolfo <nicolas.dinolfio@xpo.com>
2020-05-29 15:36:45 -0300

    agrego router de documentos

--- src/apiV1/index.ts
+++ src/apiV1/index.ts
@@ -1,10 +1,12 @@
-import { Router } from 'express';
-import auth from './auth/auth.route';
-import users from './users/user.route';
+import { Router } from "express";
+import auth from "./auth/auth.route";
+import users from "./users/user.route";
+import documento from "./documentos/documento.route";
 
 const router: Router = Router();
 
-router.use('/', auth);
-router.use('/users', users);
+router.use("/", auth);
+router.use("/users", users);
+router.use("/documento", documento);
 
 export default router;
