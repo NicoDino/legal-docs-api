ef8099046c9e078a58982be18a17cd6676713276
Nicolas Dinolfo <nicolas.dinolfio@xpo.com>
2020-07-12 20:39:25 -0300

    log node env

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,11 @@ import './config/db';
 
 const PORT = CONFIG.PORT;
 
-app.listen(PORT, err => {
+app.listen(PORT, (err) => {
   if (err) {
     return console.log(err);
   }
 
   console.log(`Server is listening on ${PORT}`);
+  console.log('SERVER RUNNING IN ', process.env.NODE_ENV);
 });
